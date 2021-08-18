dde2530824bbff9f30a677c17fb0b65d33951980
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-18 13:50:55 +0300

    completed custom date-count pipe

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -9,12 +9,12 @@ import { Goal } from '../goal';
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
 
-    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2021, 2, 14)),
-    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2021, 2, 15)),
-    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon', new Date(2021, 3, 16)),
-    new Goal(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2021, 3, 7)),
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son', new Date(2021, 11, 14)),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot', new Date(2021, 10, 15)),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon', new Date(2021, 12, 16)),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks', new Date(2021, 9, 7)),
     new Goal(5,'Solve math homework','Damn Math', new Date(2021, 3, 8)),
-    new Goal( 6, 'Plot world domination', 'Definetly on my to-do list', new Date(2021, 7, 9)),
+    new Goal( 6, 'Plot world domination', 'Definetly on my to-do list', new Date(2021, 10, 9)),
     
     
   ]; 
