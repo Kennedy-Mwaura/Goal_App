71f45ce37f4a0ff4529880222194003545b27348
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-18 13:02:45 +0300

    updated text decoration

---
+++ src/app/strikethrough.directive.ts
@@ -0,0 +1,12 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[appStrikethrough]'
+})
+export class StrikethroughDirective {
+
+  constructor(private elem:ElementRef ) { 
+    this.elem.nativeElement.style.textDecoration = 'line-through';
+  }
+
+}
