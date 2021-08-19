bb051392b62f2b030bc186d6fc0b68ab808dc464
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-08-19 12:35:24 +0300

    removed extra boilerplate

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -13,6 +13,9 @@ export class GoalDetailComponent implements OnInit {
   goalComplete(complete: boolean){
     this.isComplete.emit(complete);
   }
+  goalDelete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   constructor() { }
 
